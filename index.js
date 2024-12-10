0f896d71087cf67a8e64d7424f1f58d49bbf48f0
asavvidi <asavvidi@csd.auth.gr>
2024-12-10 14:22:20 +0200

    Finished the /submit form when we pass the name of the city and retrieve the data such as lon, lat and city state. The nect step is using the API to send the cords and retrieve the weather data

--- index.js
+++ index.js
@@ -13,11 +13,38 @@ let API_KEY = "c59d61fb5400026251cf50b3edf6f44e";
 app.get("/", (req, res) => {
   res.render("index.ejs", { wait: "Waiting for the city name..." });
 });
-app.get("/submit", (req, res) => {
-  const cityName = req.query["cityName"];
-  res.render("index.ejs", { cityName: cityName });
-  //const response = await axios.get('')
+
+app.post("/submit", async (req, res) => {
+  const cityName = req.body["cityName"];
+  try {
+    const response = await axios.get(
+      "http://api.openweathermap.org/geo/1.0/direct",
+      {
+        params: {
+          q: `${cityName},GR`,
+          appid: API_KEY,
+        },
+      }
+    );
+
+    const location = response.data[0];
+    const lat = location.lat;
+    const lon = location.lon;
+    const city = location.name;
+    const state = location.state;
+
+    res.render("index.ejs", {
+      latitude: lat,
+      longitude: lon,
+      city: city,
+      state: state,
+    });
+  } catch (error) {
+    console.error("Error fetching data from OpenWeatherMap:", error);
+    res.status(500).send("Error fetching data");
+  }
 });
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
